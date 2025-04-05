61778b2f1ab372e7084d2f3b0fcf3bc64c5f2af1
cat_in_136 <cat.in.136+github@gmail.com>
2025-04-05 15:29:06 +0900

    infotxt: Add EXIF data

--- plugins/infotxt/xviewer-infotxt-plugin.c
+++ plugins/infotxt/xviewer-infotxt-plugin.c
@@ -18,6 +18,10 @@
 #include <exempi/xmpconsts.h>
 #endif
 
+#ifndef HAVE_EXIF
+#include <xviewer/xviewer-exif-util.h>
+#endif
+
 #ifdef HAVE_LCMS
 #include <lcms2.h>
 #endif
@@ -300,6 +304,63 @@ static void manage_infotxt_data(XviewerInfotxtPlugin *plugin) {
     g_object_unref(pbuf);
   }
 
+#if HAVE_EXIF
+  ExifData *exif = xviewer_image_get_exif_info(image);
+  if (exif != NULL) {
+    static const ExifTag EXIF_TAGS[] = {
+        EXIF_TAG_DATE_TIME_ORIGINAL,
+        EXIF_TAG_DATE_TIME_DIGITIZED,
+        EXIF_TAG_MAKE,
+        EXIF_TAG_MODEL,
+        EXIF_TAG_LENS_MAKE,
+        EXIF_TAG_LENS_MODEL,
+        EXIF_TAG_SOFTWARE,
+        EXIF_TAG_EXPOSURE_TIME,
+        EXIF_TAG_FNUMBER,
+        EXIF_TAG_ISO_SPEED_RATINGS,
+        EXIF_TAG_FOCAL_LENGTH,
+        EXIF_TAG_EXPOSURE_MODE,
+        EXIF_TAG_WHITE_BALANCE,
+    };
+    gchar exif_buffer[512];
+    const gchar *buf_ptr = NULL;
+    gchar *text = NULL;
+
+    for (int i = 0; i < sizeof(EXIF_TAGS) / sizeof(EXIF_TAGS[0]); i++) {
+      buf_ptr = xviewer_exif_data_get_value(exif, EXIF_TAGS[i], exif_buffer,
+                                            sizeof(exif_buffer));
+      if (buf_ptr != NULL) {
+        if ((EXIF_TAGS[i] == EXIF_TAG_DATE_TIME_ORIGINAL ||
+             EXIF_TAGS[i] == EXIF_TAG_DATE_TIME_DIGITIZED) &&
+            buf_ptr) {
+          text = xviewer_exif_util_format_date(buf_ptr);
+        } else {
+          const size_t text_len = strnlen(buf_ptr, sizeof(exif_buffer)) + 8;
+          text = g_strndup(buf_ptr, text_len);
+          if (g_utf8_make_valid(text, text_len) == NULL) {
+            g_free(text);
+            continue;
+          }
+        }
+
+        if (g_strcmp0(text, "") != 0) {
+          const char *const tag_name = exif_tag_get_name(EXIF_TAGS[i]);
+          gtk_text_buffer_insert_with_tags_by_name(buffer, &buffer_iter,
+                                                   tag_name, -1, "key", NULL);
+          gtk_text_buffer_insert(buffer, &buffer_iter, "\n", -1);
+          gtk_text_buffer_insert(buffer, &buffer_iter, text, -1);
+          gtk_text_buffer_insert(buffer, &buffer_iter, "\n", -1);
+        }
+
+        g_free(text);
+      }
+    }
+
+    exif_data_unref(exif);
+    gtk_text_buffer_insert(buffer, &buffer_iter, "\n", -1);
+  }
+#endif
+
 #if HAVE_EXEMPI
   XmpPtr xmp_data = xviewer_image_get_xmp_info(image);
   if (xmp_data != NULL) {
@@ -344,8 +405,11 @@ static void selection_changed_cb(XviewerThumbView *view,
   g_return_if_fail(image != NULL);
 
   // If EXIF is loaded, all image metadata is assumed to be loaded.
-  if (xviewer_image_has_data(image, XVIEWER_IMAGE_DATA_EXIF)) {
-    manage_infotxt_data(plugin);
+  if (xviewer_image_get_metadata_status(image) ==
+          XVIEWER_IMAGE_METADATA_READY &&
+      xviewer_image_has_data(image, XVIEWER_IMAGE_DATA_EXIF)) {
+    // manage_infotxt_data(plugin);
+    g_idle_add((GSourceFunc)manage_infotxt_data, plugin);
   } else {
     XviewerJob *const job =
         xviewer_job_load_new(image, XVIEWER_IMAGE_DATA_EXIF);
